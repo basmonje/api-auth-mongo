962833097dd3114290a223ddeca959ddf23bfee2
basmonje <basmonje@gmail.com>
2025-01-24 23:29:49 -0300

    Corregir error en configuración de variables locales

--- src/index.js
+++ src/index.js
@@ -1,14 +1,9 @@
 import { createApp } from "./main.js";
-import { NODE_ENV, PORT } from "./utils/config.js";
-
-const config = {
-  env: NODE_ENV,
-  port: PORT,
-};
+import { config } from "./utils/config.js";
 
 const app = createApp(config);
 
-app.listen(config.port, () => {
-  const server_message = `http://localhost:${config.port}\n`;
+app.listen(config.PORT, () => {
+  const server_message = `http://localhost:${config.PORT}\n`;
   console.log("Servidor Iniciado", server_message);
 });
--- src/utils/config.js
+++ src/utils/config.js
@@ -1,6 +1,6 @@
-import { config } from "dotenv";
+import dotenv from "dotenv";
 
-config();
+dotenv.config();
 
 export const NODE_ENV = process.env.NODE_ENV || "production";
 export const PORT = process.env.PORT || 4000;
@@ -9,3 +9,9 @@ export const JWT_SECRET_APP = process.env.SECRET_PASSWORD_APP;
 export const JWT_REFRESH_SECRET_APP = process.env.REFRESH_SECRET_APP;
 
 export const URL_DATABASE = process.env.MONGODB_DATABASE;
+
+export const config = Object.freeze({
+  NODE_ENV: NODE_ENV,
+  PORT: PORT,
+  DATABASE: URL_DATABASE,
+});
